c2d89e94ccaea39a6ae038564c7091d85f571833
Aulya Yarzuki <48020037+ayarzuki@users.noreply.github.com>
2022-01-13 18:53:42 +0700

    submit-tes

---
+++ app.js
@@ -0,0 +1,33 @@
+const pokedex = document.getElementById('pokemonList');
+
+const fetchPokemon = () => {
+	const promises = [];
+	for (let i = 1; i <= 20; i++) {
+		const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+		promises.push(fetch(url).then(res => res.json()));
+	}
+	Promise.all(promises).then(results => {
+		const pokemon = results.map(result => ({
+			name: result.name.charAt(0).toUpperCase() + result.name.slice(1),
+			image: result.sprites['front_default'],
+		}));
+		displayPokemon(pokemon);
+	});
+};
+
+const displayPokemon = pokemon => {
+	console.log(pokemon);
+	const pokemonHTMLString = pokemon
+		.map(
+			pokeman => `
+        <li class="card">
+            <img class="card-image" src="${pokeman.image}"/>
+            <h2 class="card-title">${pokeman.name}</h2>
+        </li>
+    `
+		)
+		.join('');
+	pokedex.innerHTML = pokemonHTMLString;
+};
+
+fetchPokemon();
